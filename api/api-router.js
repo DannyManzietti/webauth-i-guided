01426f1070099307f17f2a41ded080bfba88bf73
DannyManzietti <danmanzietti@gmail.com>
2019-11-11 12:31:13 -0500

    password hash

--- api/api-router.js
+++ api/api-router.js
@@ -1,13 +1,25 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const bcrypt = require("bcryptjs");
 
-const authRouter = require('../auth/auth-router.js');
-const usersRouter = require('../users/users-router.js');
+const authRouter = require("../auth/auth-router.js");
+const usersRouter = require("../users/users-router.js");
+const credentials = req.body;
 
-router.use('/auth', authRouter);
-router.use('/users', usersRouter);
+const hash = bcrypt.hashSync(credentials.password, 14);
 
-router.get('/', (req, res) => {
+credentials.password = hash;
+router.use("/auth", authRouter);
+router.use("/users", usersRouter);
+
+router.get("/", (req, res) => {
   res.json({ api: "It's alive" });
+});
+router.get("/hash", (req, res) => {
+  //read a password from body
+  //hash password
+  //return to use in object that looks like {password:'original password', hash: 'hash password'}
+  if (!user || !bcrypt.compareSync(credentials.password, user.password)) {
+    return res.status(401).json({ error: 'Incorrect credentials' 
 });
 
 module.exports = router;
